766a434cc546289e116cb0a1e8dee524a3525284
Sean Birmingham <sean.birmingham98@gmail.com>
2022-07-22 02:00:37 -0400

    implemented alan ai

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 
@@ -6,23 +7,31 @@ import Movies from './pages/Movies';
 import MovieInfo from './pages/MovieInfo';
 import NavBar from './components/NavBar';
 import Profile from './pages/Profile';
+import useAlan from './components/Alan';
 
 import './App.css';
 
-const App = () => (
-  <div style={{ display: 'flex', height: '100%' }}>
-    <CssBaseline />
-    <NavBar />
-    <main style={{ flexGrow: 1, padding: '2em', width: '100%' }}>
-      <div style={{ height: '70px' }} />
-      <Routes>
-        <Route path="/" element={<Movies />} />
-        <Route path="/movie/:id" element={<MovieInfo />} />
-        <Route path="/actors/:id" element={<Actors />} />
-        <Route path="/profile/:id" element={<Profile />} />
-      </Routes>
-    </main>
-  </div>
-);
+const App = () => {
+  const alanBtnContainer = useRef();
+
+  useAlan();
+
+  return (
+    <div style={{ display: 'flex', height: '100%' }}>
+      <CssBaseline />
+      <NavBar />
+      <main style={{ flexGrow: 1, padding: '2em', width: '100%' }}>
+        <div style={{ height: '70px' }} />
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInfo />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+      <div ref={alanBtnContainer} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Alan.js
@@ -0,0 +1,48 @@
+import { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import { ColorModeContext } from '../utils/ToggleColorMode';
+import { fetchToken } from '../utils';
+import { selectGenreOrCategory, searchMovie } from '../features/movieSlice';
+
+const useAlan = () => {
+  const { setMode } = useContext(ColorModeContext);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    alanBtn({
+      key: '3fbb96e87372f3e8ae192d0f7d7f0c622e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === 'chooseGenre') {
+          const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
+
+          if (foundGenre) {
+            navigate('/');
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
+            navigate('/');
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === 'changeMode') {
+          if (mode === 'light') {
+            setMode('dark');
+          } else {
+            setMode('light');
+          }
+        } else if (command === 'login') {
+          fetchToken();
+        } else if (command === 'logout') {
+          localStorage.clear();
+          navigate('/');
+        } else if (command === 'search') {
+          dispatch(searchMovie(query));
+        }
+      },
+    });
+  }, []);
+};
+export default useAlan;
