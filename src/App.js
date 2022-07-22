ba4ad342fe0306ed082a48c2c50eef6edcf6102e
Sean Birmingham <sean.birmingham98@gmail.com>
2022-07-22 14:22:47 -0400

    fix for movies not showing when logging in

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ const App = () => {
         <div style={{ height: '70px' }} />
         <Routes>
           <Route path="/" element={<Movies />} />
+          <Route path="/approved" element={<Movies />} />
           <Route path="/movie/:id" element={<MovieInfo />} />
           <Route path="/actors/:id" element={<Actors />} />
           <Route path="/profile/:id" element={<Profile />} />
